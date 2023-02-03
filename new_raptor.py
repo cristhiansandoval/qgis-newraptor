68b79ba9934aeebf4edcf119eee179c98de45081
unknown <you@example.com>
2023-02-03 11:59:32 -0300

    Agregado de datos

--- new_raptor.py
+++ new_raptor.py
@@ -23,7 +23,7 @@
 """
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, QDate
 from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction, QMessageBox
+from qgis.PyQt.QtWidgets import QAction, QMessageBox, QTableWidgetItem
 
 from qgis.core import QgsProject, QgsFeature, QgsGeometry, QgsPoint
 
@@ -206,6 +206,8 @@ class NewRaptor:
             missing_layers.append("Raptor Nests")
         if not "Raptor Buffer" in map_layers:
             missing_layers.append("Raptor Buffer")
+        if not "Linear Buffer" in map_layers:
+            missing_layers.append("Linear Buffer")
         if missing_layers:
             msg = "Las siguientes capas estan perdidas para este proyecto \n"
             for lyr in missing_layers:
@@ -224,6 +226,7 @@ class NewRaptor:
             # substitute with your code.
             lyrNests = QgsProject.instance().mapLayersByName('Raptor Nests')[0]
             lyrBuffer = QgsProject.instance().mapLayersByName('Raptor Buffer')[0]
+            lyrLinear = QgsProject.instance().mapLayersByName('Linear Buffer')[0]
             idxNestID = lyrNests.fields().indexOf('Nest_ID')
             valNestID = lyrNests.maximumValue(idxNestID) + 1
             valLat = self.dlg.spbLatitude.value()
@@ -254,9 +257,23 @@ class NewRaptor:
             lyrBuffer.reload()
 
             dlgTable = DlgTable()
+            # Encuentra proyectos lineales que se verán afectados e infórmelos en la tabla
+            linears = lyrLinear.getFeatures()
+            for linear in linears:
+                valID = linear.attribute("Project")
+                valType = linear.attribute("type")
+                valDistance = linear.geometry().distance(geom)
+                if valDistance < valBuffer:
+                    # Rellena tabla con datos lineales
+                    row = dlgTable.tblImpacts.rowCount()
+                    dlgTable.tblImpacts.setItem(row, 0, QTableWidgetItem(str(valID)))
+                    dlgTable.tblImpacts.setItem(row, 1, QTableWidgetItem(str(valType)))
+                    twi = QTableWidgetItem("{:4.5f}".format(valDistance))
+                    twi.setTextAlignment(QtCore.Qt.AlignRight)
+                    dlgTable.tblImpacts.setItem(row, 2, twi)
             dlgTable.show()
             dlgTable.exec_()
-
+            
         else:
             QMessageBox.information(self.dlg, "Message", "Deberia ejecutar solo con Cancel apretado")
     
